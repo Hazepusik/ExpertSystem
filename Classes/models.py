696150bbf2a2811188c3b974f9e3803c2541c4b2
Hazepusik <hazepusik@rambler.ru>
2014-11-03 13:33:39 +0400

    заготовка моделей предметной области

---
+++ Classes/models.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from datetime import date, datetime
+
+
+class Situation(models.Model):
+    name = models.CharField(max_length=100, default="")
+    description = models.CharField(max_length=500, default="")
+    globalGroupId = models.IntegerField(blank=True, null=True)
+    properties = models.ManyToManyField(Recommendation, null=True, through="ProductPropertyValue")
+    variationOf = models.ForeignKey('Modification', null=True, blank=True)
+
+
+class Recommendation(models.Model):
+    name = models.CharField(max_length=100, default="")
+    description = models.CharField(max_length=500, default="")
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=500)
+    situation = models.ForeignKey('Situation')
+
+
+class Answer(models.Model):
+    name = models.CharField(max_length=500)
+    question = models.ForeignKey('Question')
+
+
+class Condition(models.Model):
+    answer = models.ForeignKey('Answer')
+    conditionSet = models.ForeignKey('ConditionSet')
+    isPositive = models.BooleanField(default=True)
+
+
+class ConditionSet(models.Model):
+    conditionSet = models.ForeignKey('Recommendation')
