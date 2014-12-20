d26e6a4c78ae01d03c26fd686743a46ecf7f067e
Martolod Slaaf <mart@Veresk.(none)>
2014-12-20 11:22:49 +0300

    Panic. Templating.

--- ExpertSystem/urls.py
+++ ExpertSystem/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import settings
 from Classes.models import test
+from Classes.views import main, situations
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ExpertSystem.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^test/$',  test),
+    url(r'^$',  main),
+    url(r'^scenarios/$',  situations),
     url(r'^admin/', include(admin.site.urls)),
 )
 urlpatterns += patterns('',
